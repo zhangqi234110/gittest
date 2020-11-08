6ad4d919bd8034360a63afb5a80b5fc35a09729f
zhangqi234110 <314890945@qq.com>
2020-11-08 11:51:21 +0800

    1

--- src/main/java/com/example/gittest/Hello.java
+++ src/main/java/com/example/gittest/Hello.java
@@ -2,6 +2,6 @@ package com.example.gittest;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("nihao");
+        System.out.println("nihao321");
     }
 }
